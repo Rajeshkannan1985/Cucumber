9e905cf21b72c89e439218d97bce702b8d6318a2
Rajeshkannan1985 <kannanearly@gmail.com>
2019-09-16 06:36:02 +0530

    Code Added

---
+++ target/report.js
@@ -0,0 +1,211 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/user3/eclipse-workspace/Cucumber14/src/test/resources/Feature/AddTariff.feature");
+formatter.feature({
+  "name": "Add Tariff Plan in two dimensional",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user should be in the Telecome homepages",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_in_the_Telecome_homepages()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user should be click on Add Tariffs",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_click_on_Add_Tariffs()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should be fill all the field with valid dataa",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_fill_all_the_field_with_valid_dataa()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user should be click on submit buttons",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_click_on_submit_buttons()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Add Tariff should be displayedd",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AddTariff2D.add_Tariff_should_be_displayedd()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user should be in the Telecome homepages",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_in_the_Telecome_homepages()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user should be click on Add Tariffs",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_click_on_Add_Tariffs()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should be fill all the field with valid dataas",
+  "rows": [
+    {
+      "cells": [
+        "100",
+        "100",
+        "100",
+        "100",
+        "100",
+        "100",
+        "1"
+      ]
+    }
+  ],
+  "keyword": "When "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_fill_all_the_field_with_valid_dataas(DataTable)"
+});
+formatter.result({
+  "error_message": "java.lang.IndexOutOfBoundsException: Index: 1, Size: 1\r\n\tat java.util.ArrayList.rangeCheck(Unknown Source)\r\n\tat java.util.ArrayList.get(Unknown Source)\r\n\tat java.util.Collections$UnmodifiableList.get(Unknown Source)\r\n\tat org.Cucumber14.AddTariff2D.user_should_be_fill_all_the_field_with_valid_dataas(AddTariff2D.java:45)\r\n\tat ✽.User should be fill all the field with valid dataas(C:/Users/user3/eclipse-workspace/Cucumber14/src/test/resources/Feature/AddTariff.feature:13)\r\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "user should be click on submit buttons",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_click_on_submit_buttons()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Add Tariff should be displayedd",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AddTariff2D.add_Tariff_should_be_displayedd()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenario({
+  "name": "",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user should be in the Telecome homepages",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_in_the_Telecome_homepages()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user should be click on Add Tariffs",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_click_on_Add_Tariffs()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should be fill all the field with valid dataas.",
+  "rows": [
+    {
+      "cells": [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g"
+      ]
+    },
+    {
+      "cells": [
+        "100",
+        "100",
+        "100",
+        "100",
+        "100",
+        "100",
+        "1"
+      ]
+    }
+  ],
+  "keyword": "When "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_fill_all_the_field_with_valid_datas1(DataTable)"
+});
+formatter.result({
+  "error_message": "java.lang.IndexOutOfBoundsException: Index: 1, Size: 1\r\n\tat java.util.ArrayList.rangeCheck(Unknown Source)\r\n\tat java.util.ArrayList.get(Unknown Source)\r\n\tat java.util.Collections$UnmodifiableList.get(Unknown Source)\r\n\tat org.Cucumber14.AddTariff2D.user_should_be_fill_all_the_field_with_valid_datas1(AddTariff2D.java:56)\r\n\tat ✽.User should be fill all the field with valid dataas.(C:/Users/user3/eclipse-workspace/Cucumber14/src/test/resources/Feature/AddTariff.feature:21)\r\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "user should be click on submit buttons",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "AddTariff2D.user_should_be_click_on_submit_buttons()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Add Tariff should be displayedd",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "AddTariff2D.add_Tariff_should_be_displayedd()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
--- target/test-classes/Feature/AddTariff.feature
+++ target/test-classes/Feature/AddTariff.feature
@@ -1,8 +1,25 @@
-Feature: AddTariff
+Feature: Add Tariff Plan in two dimensional
 
-  Scenario: AddTariffPlan
-    Given User in the Telecome homepage
-    And  click on Add Tariff
-    When User should fill all the field this valid data
-    And user clicks on submit button
-    Then Customer ID should be displayed
+  Scenario: 
+    Given user should be in the Telecome homepages
+    And user should be click on Add Tariffs
+    When User should be fill all the field with valid dataa
+    And user should be click on submit buttons
+    Then Add Tariff should be displayedd
+
+  Scenario: 
+    Given user should be in the Telecome homepages
+    And user should be click on Add Tariffs
+    When User should be fill all the field with valid dataas
+      | 100 | 100 | 100 | 100 | 100 | 100 | 1 |
+    And user should be click on submit buttons
+    Then Add Tariff should be displayedd
+
+  Scenario: 
+    Given user should be in the Telecome homepages
+    And user should be click on Add Tariffs
+    When User should be fill all the field with valid dataas.
+      | a   | b   | c   | d   | e   | f   | g |
+      | 100 | 100 | 100 | 100 | 100 | 100 | 1 |
+    And user should be click on submit buttons
+    Then Add Tariff should be displayedd
